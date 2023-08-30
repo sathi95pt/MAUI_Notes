e780bdde69b31d3557c52cd8d004c4dbb7b6121e
sathi95pt <41156907+sathi95pt@users.noreply.github.com>
2023-08-30 11:21:05 +0530

    Sathya Notes

---
+++ SathyaNotes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SathyaNotes;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        Routing.RegisterRoute(nameof(Views.NotesPage), typeof(Views.NotesPage));
+    }
+}
---
+++ SathyaNotes/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SathyaNotes;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
